a8c59dbfaf2ad6462f596ac95580d3edfdbb72ae
zhangxianglong <zhangxianglong@bytedance.com>
2022-03-17 14:54:21 +0800

    增加回调哈桉树

--- cgo_interface/initPublic.go
+++ cgo_interface/initPublic.go
@@ -10,31 +10,66 @@ extern void connectCallback(uint16_t,char *);
 extern void disconnectCallback(uint16_t,char *);
 */
 import "C"
-import "viplex/common"
-import "encoding/json"
-import "unsafe"
+import (
+	"encoding/json"
+	"fmt"
+	"unsafe"
+	"viplex/common"
+)
 
 //公网初始化函数
-func InitPublicNet()error{
-	pn:=common.PublicNet{
-		PublicModule:true,
-		Encrypt:true,
-		CertificatePath:"/root/go/src/viplex/ViplexCoreCentOS/bin/cert",
-		Port:16603,
-		HttpserverUrl:"localhost:80",
-		HttpserverLocalPath:"",
+func InitPublicNet() error {
+	pn := common.PublicNetInput{
+		PublicModule:        true,
+		Encrypt:             true,
+		CertificatePath:     "/root/go/src/viplex/ViplexCoreCentOS/bin/cert",
+		Port:                16603,
+		HttpserverUrl:       "localhost:80",
+		HttpserverLocalPath: "",
 	}
-	pnjson,_:=json.Marshal(pn)
-	pnjsonC:=C.CString(string(pnjson))
+	pnjson, _ := json.Marshal(pn)
+	pnjsonC := C.CString(string(pnjson))
 
-	C.nvInitPublicNetAsync(pnjsonC,(C.ExportViplexCallback)(C.connectCallback),(C.ExportViplexCallback)(C.disconnectCallback))
+	C.nvInitPublicNetAsync(pnjsonC, (C.ExportViplexCallback)(C.connectCallback), (C.ExportViplexCallback)(C.disconnectCallback))
 	C.free(unsafe.Pointer(pnjsonC))
 	return nil
 }
 
 //export connectCallback
-func connectCallback(C.uint16_t,*C.char){
-	fmt.Println("")
+func connectCallback(code C.uint16_t, result *C.char) {
+	fmt.Println("connect Callback")
+	retCode := int(code)
+	resultJson := C.GoString(result)
+
+	if retCode != 0 {
+		fmt.Println("device connect failed")
+		return
+	}
+	fmt.Println("deivce connect success")
+	ret := &common.PublicNetOutput{}
+	err := json.Unmarshal([]byte(resultJson), ret)
+	if err != nil {
+		fmt.Println("connectCallback input json data error,err:", err)
+		return
+	}
+
 }
+
 //export disconnectCallback
-func disconnectCallback(C.uint16_t,*C.char){}
+func disconnectCallback(code C.uint16_t, result *C.char) {
+	fmt.Println("disconnect Callback")
+	retCode := int(code)
+	resultJson := C.GoString(result)
+
+	if retCode != 0 {
+		fmt.Println("device disconnect failed")
+		return
+	}
+	fmt.Println("device disconnect success")
+	ret := &common.PublicNetOutput{}
+	err := json.Unmarshal([]byte(resultJson), ret)
+	if err != nil {
+		fmt.Println("disconnectCallback input json data error,err:", err)
+		return
+	}
+}
--- common/common.go
+++ common/common.go
@@ -10,11 +10,36 @@ type Programs struct {
 	SN string `json:"sn"`
 }
 
-type PublicNet struct{
-	PublicModule bool `json:"publicmodule"`
-	Encrypt bool `json:"encrypt"`
-	CertificatePath string `json:"certificatePath"`
-	Port int `json:"port"`
-	HttpserverUrl string `json:"httpserverUrl"`
+//初始化公网链接的入参
+type PublicNetInput struct {
+	PublicModule        bool   `json:"publicmodule"`
+	Encrypt             bool   `json:"encrypt"`
+	CertificatePath     string `json:"certificatePath"`
+	Port                int    `json:"port"`
+	HttpserverUrl       string `json:"httpserverUrl"`
 	HttpserverLocalPath string `json:"httpserverLocalPath"`
+}
+
+//初始化公网链接的出参
+type PublicNetOutput struct {
+	Sn            string   `json:"sn"`
+	ConnectId     string   `json:"connectid"`
+	ProductName   string   `json:"productName"`
+	Width         int      `json:"width"`
+	Height        int      `json:"heigth"`
+	Rotation      int      `json:"rotation"`
+	AliasName     string   `json:"aliasName"`
+	Logined       bool     `json:"logined"`
+	Username      []string `json:"username"`
+	TcpPort       int      `json:"tcpPort"`
+	FtpPort       int      `json:"ftpPort"`
+	SyssetFtpPort int      `json:"syssetFtpPort"`
+	SyssetTcpPort int      `json:"syssetTcpPort"`
+	Key           string   `json:"key"`
+	Platform      string   `json:"platform"`
+	Privacy       bool     `json:"privacy"`
+	Password      string   `json:"password"`
+	TerminalState int      `json:"terminalState"`
+	IgnoreTime    int64    `json:"ignoreTime"`
+	HasPassword   bool     `json:"hasPassword"`
 }
